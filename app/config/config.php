4076c45d61e6eb4f1f0e4a9989843d1766e00130
dedoa82 <67242566+dedoa82@users.noreply.github.com>
2021-02-03 00:04:00 +0200

    Add files via upload

--- app/config/config.php
+++ app/config/config.php
@@ -1,9 +1,9 @@
 <?php
     //Database params
-    define('DB_HOST', 'localhost'); //Add your db host
-    define('DB_USER', 'root'); // Add your DB root
-    define('DB_PASS', '123456'); //Add your DB pass
-    define('DB_NAME', 'blog'); //Add your DB Name
+    define('DB_HOST', 'localhost'); //Add DB Host
+    define('DB_USER', 'root');      //Add DB Root
+    define('DB_PASS', '123456');    //Add DB Password
+    define('DB_NAME', 'blog');      //Add DB Name
 
     //APPROOT
     define('APPROOT', dirname(dirname(__FILE__)));
@@ -12,4 +12,4 @@
     define('URLROOT', 'http://localhost/Blog');
 
     //Sitename
-    define('SITENAME', 'Login & Register script');
+    define('SITENAME', 'Simple Blog');
